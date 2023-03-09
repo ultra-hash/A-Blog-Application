94adc5c77db9db675ca4a79bdf8ba237c897e8d9
ultra-hash <jarviscommand3@gmail.com>
2023-03-09 23:36:53 +0530

    updated the post_detail view with shortcut from django.shortcuts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
 
@@ -9,10 +9,9 @@ def post_list(request):
                   {'posts': posts})
 
 def post_detail(request, id):
-    try:
-        post: Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("No Post found.")
+    post = get_object_or_404(Post, 
+                             id=id, 
+                             status=Post.Status.PUBLISHED)
     
     return render(request,
                   'blog/post/detail.html',
