3e2fc691b263076f73f15e6975862d4cd603e694
ultra-hash <jarviscommand3@gmail.com>
2023-03-11 23:33:47 +0530

    added pagination.html and modified post_list view with pagination functionality

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404
+from django.core.paginator import Paginator
 
 def post_list(request):
     posts = Post.published.all()
+    # pagination with 3 posts per page
+    paginator = Paginator(posts, 2)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
+
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
