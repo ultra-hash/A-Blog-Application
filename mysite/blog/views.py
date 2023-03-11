030a372716c2820d8ac1dbc50c5713f7a507cd0d
ultra-hash <jarviscommand3@gmail.com>
2023-03-11 23:39:41 +0530

    handeled exception for pagination with invalid page number

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage
 
 def post_list(request):
     posts = Post.published.all()
     # pagination with 3 posts per page
     paginator = Paginator(posts, 2)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
-
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        # if page_number is out of range deliver last page of reuslts
+        posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
