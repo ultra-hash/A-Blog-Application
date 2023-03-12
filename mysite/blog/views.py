52e747cb6943456b6c477322fab0ef2502de1925
ultra-hash <jarviscommand3@gmail.com>
2023-03-12 23:14:58 +0530

    using class based view to list posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 2
+    template_name = 'blog/post/list.html'
 
 def post_list(request):
     posts = Post.published.all()
